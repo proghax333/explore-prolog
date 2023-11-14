d90971c9ed8c2f4d3cfec3a7a72a2ddd07dc4b03
proghax333 <skillzworldtech@gmail.com>
2023-11-14 18:00:59 +0530

    docs: add program execution's output

--- practicals/eight_queens.pl
+++ practicals/eight_queens.pl
@@ -19,3 +19,22 @@ safe_queens([Q|Qs], Q0, D0) :-
         abs(Q0 - Q) #\= D0,
         D1 #= D0 + 1,
         safe_queens(Qs, Q0, D1).
+
+go :-
+        eight_queens(Qs),
+        write(Qs).
+
+/*
+Output:
+
+?- go.
+[1,5,8,6,3,7,2,4]
+true ;
+[1,6,8,3,7,4,2,5]
+true ;
+[1,7,4,6,8,2,5,3]
+true ;
+[1,7,5,8,2,4,6,3]
+true .
+
+*/
